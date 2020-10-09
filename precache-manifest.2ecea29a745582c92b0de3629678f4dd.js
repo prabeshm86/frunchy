9848f2c48f916611a1f5f4b2b924a0cf9ca98c41
Prabesh <manandharprabesh@gmail.com>
2020-10-09 07:33:31 -0700

    Updates

---
+++ precache-manifest.2ecea29a745582c92b0de3629678f4dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3d3b44966739f9893658b7bdfa4a17",
+    "url": "/frunchy/index.html"
+  },
+  {
+    "revision": "4b81b9ccf5770868be31",
+    "url": "/frunchy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "50eac1744f1335f053ad",
+    "url": "/frunchy/static/js/2.96e5e7e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frunchy/static/js/2.96e5e7e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b81b9ccf5770868be31",
+    "url": "/frunchy/static/js/main.0f39cb43.chunk.js"
+  },
+  {
+    "revision": "61ee67070caf88ad974e",
+    "url": "/frunchy/static/js/runtime-main.07322e61.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frunchy/static/media/logo.5d5d9eef.svg"
+  }
+]);
