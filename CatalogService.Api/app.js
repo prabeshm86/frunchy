8e8a2e80f742aee0485e568be1c9dd07396b2cfe
Prabesh <manandharprabesh@gmail.com>
2020-10-11 14:12:17 -0700

    Added CatalogService API project

---
+++ CatalogService.Api/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const jwt = require("jsonwebtoken");
+process.env.ACCESS_TOKEN_SECRET = "XCAP05H6LoKvbRRa/QkqLNMI7cOHguaRyHzyg7n5qEkGjQmtBhz4SzYh4Fqwjyi3KJHlSXKPwVu2+bXr6CtpgQ=="
+
+function authenticateToken(req, res, next) {
+    // Gather the jwt access token from the request header
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401) // if there isn't any token
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      console.log(err)
+      if (err) return res.sendStatus(403)
+      req.user = user
+      next() // pass the execution off to whatever request the client intended
+    })
+}
+
+app.get('/', authenticateToken, (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
